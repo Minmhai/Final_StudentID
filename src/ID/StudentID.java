b64969525b8eb9cd9800a4a485bfb6557fe07349
Napasorn Tanchittiwat <napasorn.tctw@gmail.com>
2021-12-02 16:20:06 +0700

    first commit

---
+++ src/ID/StudentID.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ID;
+
+/**
+ *
+ * @author napas
+ */
+public class StudentID {
+    private int id;
+  
+    public StudentID() {
+    }
+    public StudentID(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+    
